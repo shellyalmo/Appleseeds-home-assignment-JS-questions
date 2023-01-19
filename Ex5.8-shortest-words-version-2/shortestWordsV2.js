ed738484e6d658633db28c927172401e0a71f272
shellyalmo <shellyalmo@gmail.com>
2023-01-19 20:42:20 +0200

    first commit

---
+++ Ex5.8-shortest-words-version-2/shortestWordsV2.js
@@ -0,0 +1,24 @@
+// Given a string of words, return the longest word[s].
+// String will never be empty and you do not need to account for different data types.
+
+function longestWords(string) {
+  const splitted = string.split(/[\W]/); //splits string by any non alphabetic character
+  let wordsLength = {};
+  splitted.forEach((word) => {
+    if (word.length > 0) {
+      wordsLength[word] = word.length;
+    }
+  });
+
+  const longest = Math.max(...Object.values(wordsLength));
+  return Object.keys(wordsLength).filter((key) => wordsLength[key] === longest);
+}
+
+console.assert(
+  ["how", "are", "you"] === longestWords("hi how are you?"),
+  JSON.stringify(longestWords("hi how are you?"))
+);
+console.assert(
+  ["name"] === longestWords("hi my name is:"),
+  JSON.stringify(longestWords("hi my name is:"))
+);
