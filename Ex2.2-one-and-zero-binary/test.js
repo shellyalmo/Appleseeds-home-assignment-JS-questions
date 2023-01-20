19938b2f49f2fd8ab76753969389822ee164faba
shellyalmo <shellyalmo@gmail.com>
2023-01-20 13:09:33 +0200

    first commit

---
+++ Ex2.2-one-and-zero-binary/test.js
@@ -0,0 +1,20 @@
+const { binaryToInt } = require("./oneAndZeroBinary");
+const test = require("node:test");
+const assert = require("node:assert");
+
+// run with node --test --watch .\shortestWords.test.js
+[
+  [[0, 0, 0, 1], 1],
+  [[0, 0, 1, 0], 2],
+  [[0, 1, 0, 1], 5],
+  [[1, 0, 0, 1], 9],
+  [[0, 0, 1, 0], 2],
+  [[0, 1, 1, 0], 6],
+  [[1, 1, 1, 1], 15],
+  [[1, 0, 1, 1], 11],
+  [[0, 1, 0, 1, 0, 1, 0, 1, 0], 170],
+].forEach(([input, output]) => {
+  test(`${input} should be ${output}`, () => {
+    assert.deepEqual(binaryToInt(input), output);
+  });
+});
